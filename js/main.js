7679c36741ea784fcddd4490cc9f6f149256325f
Pietro <pietrospitella@gmail.com>
2021-07-21 17:25:47 +0200

    PS: class correction

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
 // 1. chiedi all’utente il cognome
+
 var yourSurn = prompt("Inserisci il tuo cognome");
+
+yourSurn = yourSurn.charAt(0).toUpperCase() + yourSurn.slice(1).toLowerCase();
+
 // 2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+
 var listOfSurns = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"]
+
 listOfSurns.push(yourSurn);
+
 // 3. stampa la lista ordinata alfabeticamente
+
 console.log(listOfSurns.sort());
+
 // 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+
+
 for (var i = 0; i < listOfSurns.length; i++) {
     console.log(listOfSurns[i]);
 }
+
 console.log(listOfSurns.indexOf(yourSurn)+1);
